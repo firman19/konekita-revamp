826d79e5fd586d3f5a496ae750b3eedda965643a
Adi Firman Ilham <firman_bjs@ymail.com>
2021-01-15 23:49:16 +0700

    [UPDATE] Adding planet icons

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,8 +9,22 @@
 </head>
 <body>
     <div class="container-fluid border border-primary part-a">
-        <div id="back-home"></div>
+        <div id="header" class="d-flex align-items-center">
+            <button class="btn btn-warning item">LOGO</button>
+            <button class="btn btn-warning item">SIAPA KAMI</button>
+            <div class="ml-auto">
+                <button class="btn btn-warning item">REGISTER</button>
+                <button class="btn btn-warning item">LOGIN</button>
+            </div>
+        </div>
         <div id="astro-1"></div>
+        <div id="planet-1"></div>
+        <div id="planet-2"></div>
+        <div id="planet-3"></div>
+        <div id="text" class="text-right">
+            <h1 class="text-white" id="slogan">TEMUKAN VENDOR KREATIF & INFLUENCERS </br> UNTUK BISNISMU</h2>
+            <button class="btn btn-primary">DAFTAR SEKARANG</button>
+        </div>
     </div>
     <script src="/js/app.js"></script>
 </body>
